3f00bb6bbcb3ba29e69a3005ba0fbbd2a4cedc59
alberto.garcia.garabal.contractor <alberto.garcia.garabal.contractor@bbva.com>
2020-04-03 19:28:29 +0200

    Added basic scaffolding for Node and Express

---
+++ src/node-api/src/index.js
@@ -0,0 +1,28 @@
+const PORT = 3000;
+
+import express from 'express';
+// const bodyParser = require('body-parser');
+
+import makeCallback from './express-callback';
+
+const app = express();
+
+// app.use(bodyParser.json())
+
+// app.post(`${apiRoot}/comments`, makeCallback(postComment))
+// app.delete(`${apiRoot}/comments/:id`, makeCallback(deleteComment))
+// app.delete(`${apiRoot}/comments`, makeCallback(deleteComment))
+// app.patch(`${apiRoot}/comments/:id`, makeCallback(patchComment))
+// app.patch(`${apiRoot}/comments`, makeCallback(patchComment))
+// app.get(`${apiRoot}/comments`, makeCallback(getComments))
+// app.use(makeCallback(notFound));
+
+
+// listen for requests
+app.listen(PORT, () => {
+  console.log('Server Node.js + Express is listening on port 3000')
+});
+
+
+export default app;
+
