8f88820197902656291f574c55bd91a6a41e7158
alberto.garcia@bluetab.net <alberto.garcia@bluetab.net>
2020-05-28 19:34:04 +0200

    Improving performance #57

--- src/node-api/src/adapters/data-access/index.js
+++ src/node-api/src/adapters/data-access/index.js
@@ -1,32 +1,29 @@
-import mongoose from 'mongoose';
 import makeDbOperations from './db-operations';
+import mongoose from 'mongoose';
 
-const mongoURI = "mongodb://localhost:27017/todotoday";
+const mongoURI = "mongodb://localhost:27017/todotoday?poolSize=4";
 const options = {
     keepAlive: true,
     keepAliveInitialDelay: 300000,
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
-
-export async function makeDb() {
-  mongoose.connect(mongoURI, options);
-  return mongoose.connection;
-};
+mongoose.connect(mongoURI, options);
+console.log("Database connection succeeded");
 
 mongoose.connection.on('error', (err)=>{
-    console.log('handle mongo errored connections: ' + err);
+  console.log('handle mongo errored connections: ' + err);
 });
 
-mongoose.connection.on('disconnected', ()=>{
-    console.log('Mongoose default connection disconnected');
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected');
 });
 
 process.on('SIGINT', () => {
-    mongoose.connection.close();
-    process.exit(0);
+  mongoose.connection.close();
+  process.exit(0);
 });
 
-const db = makeDbOperations({ makeDb });
+const db = makeDbOperations();
 
 export default db;
