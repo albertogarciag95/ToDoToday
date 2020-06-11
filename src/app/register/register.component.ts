47a3cf2f73e7ca2208d04b27f8d7dca05145d2d5
alberto.garcia@bluetab.net <alberto.garcia@bluetab.net>
2020-06-11 21:57:00 +0200

    Validations and styles #69

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  nameFormControl = new FormControl('', [ Validators.required ]);
+  userNameFormControl = new FormControl('', [ Validators.required ]);
+  dateFormControl = new FormControl('date', [ Validators.required ]);
+  emailFormControl = new FormControl('', [ Validators.required, Validators.email]);
+  passFormControl = new FormControl('', [ Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/) ]);
+  pass2FormControl = new FormControl('', [ Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/) ]);
+
+  date: any;
+
+  dateFilter = (date: Date | null): boolean => {
+    return new Date() > date;
+  }
+
   constructor() { }
+
+  validateDate(pickerInput: string): string {
+    if (!pickerInput.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/) || this.date > new Date()) {
+      return 'Por favor, introduce una fecha con formato válido';
+    }
+  }
+
+  addEvent(event: MatDatepickerInputEvent<Date>) {
+    this.date = event.value;
+  }
 
   addNewUser(){}
 
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -7,9 +7,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule, MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatNativeDateModule, MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS, ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { MatMomentDateModule, MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 import { MatDialogModule } from '@angular/material/dialog';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', component: RegisterComponent }
@@ -26,16 +28,19 @@ const routes: Routes = [
     MatNativeDateModule,
     MatMomentDateModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [RouterModule],
   providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'ja-JP' },
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' },
     {
       provide: DateAdapter,
       useClass: MomentDateAdapter,
       deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
     },
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher},
     {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
   ]
 })
